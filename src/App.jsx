1379597e39fbb852c7de029c2789ba61f695d4b0
jorge <j0rg3e93@gmail.com>
2023-11-26 11:52:24 +0100

    Se ajuste el contenido para app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import './App.css'
+import "./App.css";
+import { Header } from "./common/Header/Header";
+import { Body } from "./pages/Body/Body";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
-  
-
   return (
-    <>    
-      
+    <>
+      <Header />
+      <Body />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
