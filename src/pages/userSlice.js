94df01b9a2c84a8e157b9f985f36215f64f89bb1
jorge <j0rg3e93@gmail.com>
2023-11-26 11:37:12 +0100

    Se crea los archivos necesarios para poder user next

---
+++ src/pages/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: {
+      credentials: {}
+    },
+    reducers: {
+      login: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      },
+      logout: (state, action) => {
+        return {
+          ...state,
+          ...action.payload
+        }
+      }
+      
+    }
+    
+});
+
+export const { login, logout } = userSlice.actions;
+export const userDetails = (state) => state.user;
+export default userSlice.reducer;
