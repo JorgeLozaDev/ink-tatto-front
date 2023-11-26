af3828ec3b1cac4c78fac24380ef84b39938cb2d
jorge <j0rg3e93@gmail.com>
2023-11-26 11:44:41 +0100

    Se hacen las funciones necesarias, para usar hacer las llamadas a la api

---
+++ src/services/apiCalls.js
@@ -0,0 +1,50 @@
+import axios from "axios";
+
+const URL = "http://localhost:3000/";
+
+export const loginUser = async (endpoint, data) => {
+  const login = await axios.post(`${URL}${endpoint}`, {
+    email: data.email,
+    password: data.password,
+  });
+  return login;
+};
+
+export const insertUser = async (endpoint, data) => {
+  const inUser = await axios.post(`${URL}${endpoint}`, {
+    name: data.name,
+    lastname: data.lastname,
+    email: data.email,
+    username: data.username,
+    password: data.password,
+    birthday: data.birthday,
+  });
+  return inUser;
+};
+
+export const profileUser = async (endpoint, token) => {
+  
+  const dataUser = await axios.get(`${URL}${endpoint}`, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  return dataUser;
+};
+
+export const updateProfile = async (endpoint, token, data) => {
+  const info = {
+    name: data.name,
+    lastname: data.lastname,
+    email:data.email,
+    username: data.username,
+    birthday: data.birthday,
+  };
+  // console.log(info)
+  const headers = {
+    Authorization: "Bearer " + token,
+  };
+  
+  const updateUser = await axios.put(`${URL}${endpoint}`, info, { headers });
+  return updateUser;
+};
