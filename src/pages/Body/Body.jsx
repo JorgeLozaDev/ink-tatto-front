d84fbaf03479e616f17e2c45a5fb06872d146e2e
jorge <j0rg3e93@gmail.com>
2023-11-27 17:45:19 +0100

    Se crea los archivos para mostrar las citas

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -4,6 +4,7 @@ import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
 import { Profile } from "../Profile/Profile";
+import { ListMeetings } from "../ListMeetings/ListMeetings";
 
 export const Body = () => {
   return (
@@ -13,6 +14,7 @@ export const Body = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/singup" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
+      <Route path="/meetings" element={<ListMeetings />} />
     </Routes>
   );
 };
