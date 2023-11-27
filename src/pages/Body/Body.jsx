6a98691c2275a047f5ebb75465111245572e3479
jorge <j0rg3e93@gmail.com>
2023-11-27 23:43:15 +0100

    Se hace un cambio de nombre a las mettings, y se crea la carpeta de CreateMettings y se añade su ruta al body

--- src/pages/Body/Body.jsx
+++ src/pages/Body/Body.jsx
@@ -1,10 +1,10 @@
-
 import { Routes, Route, Navigate } from "react-router-dom";
 import { Home } from "../Home/Home";
 import { Login } from "../Login/Login";
 import { Register } from "../Register/Register";
 import { Profile } from "../Profile/Profile";
-import { ListMeetings } from "../ListMeetings/ListMeetings";
+import { ListMeetings } from "../ListMettings/ListMettings";
+import { CreateMettings } from "../CreateMettings/CreateMettings";
 
 export const Body = () => {
   return (
@@ -14,7 +14,8 @@ export const Body = () => {
       <Route path="/login" element={<Login />} />
       <Route path="/singup" element={<Register />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/meetings" element={<ListMeetings />} />
+      <Route path="/mettings" element={<ListMeetings />} />
+      <Route path="/addMettings" element={<CreateMettings />} />
     </Routes>
   );
 };
